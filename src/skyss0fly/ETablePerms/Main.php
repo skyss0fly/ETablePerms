d9564f4d73314f71faa5ab15abc4b2f2624307c9
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-18 21:01:23 +1000

    Update Main.php

--- src/skyss0fly/ETablePerms/Main.php
+++ src/skyss0fly/ETablePerms/Main.php
@@ -10,12 +10,13 @@ class Main extends PluginBase {
 public EnchantingTable $table; 
     
 public function oninterract(PlayerInteractEvent $event) {
-$block = $evnet->getBlock();
+$block = $event->getBlock();
   if ($block === $table) {
 $player = $event->getPlayer();
     if ($player->hasPermission("ETablePerms.Use")){
 
 # null
+    }
       else {
 $event->cancel();
       $player->sendMessage("Error: you lack permission to use this!")l
