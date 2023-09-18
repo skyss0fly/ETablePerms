71a93e72ca30035eb32c46a78c481151ffbab50c
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-18 21:14:13 +1000

    Update Main.php

--- src/skyss0fly/ETablePerms/Main.php
+++ src/skyss0fly/ETablePerms/Main.php
@@ -6,12 +6,16 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\block\EnchantingTable;
 use pocketmine\plugin\PluginBase;
 class Main extends PluginBase {
-
+    /*
+@param PlayerInteractEvent $event
+@priority HIGHEST
+@ignoreCancelled true*/
     
-public function oninterract(PlayerInteractEvent $event, EnchantingTable $table) {
+public function onInterract(PlayerInteractEvent $event) {
 $block = $event->getBlock();
-  if ($block === $table) {
+  if ($block instanceof Enchantingtable) {
 $player = $event->getPlayer();
+      $this->ignoreCancelled
     if ($player->hasPermission("ETablePerms.Use")){
 
 # null
