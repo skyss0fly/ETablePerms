ba22145f4393392072d1139655d429bf64e09ee7
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-18 20:55:56 +1000

    Create Main.php

---
+++ src/skyss0fly/ETablePerms/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace skyss0fly\ETablePerms;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\block\EnchantingTable;
+use pocketmine\plugin\PluginBAW;
+class Main extends PluginBase {
+
+public EnchantingTable = $table 
+    
+public function oninterract(PlayerInteractEvent $event) {
+$block = $evnet->getBlock();
+  if ($block === $table) {
+$player = $event->getPlayer();
+    if ($player->hasPermission("ETablePerms.Use")){
+
+# null
+      else {
+$event->cancel();
+      $player->sendMessage("Error: you lack permission to use this!")l
+      }
+
+    else {
+
+    #null
+  }
+}
+
+
+}
